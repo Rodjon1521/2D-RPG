75f2480a0cd2bab2ce4004438d866da775095052
Rodjon1521 <rodiongajvoronskij@gmail.com>
2024-03-18 02:28:39 +0300

    GameRunner

---
+++ Assets/_Scripts/Infrastructure/GameRunner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _Scripts.Infrastructure
+{
+    public class GameRunner : MonoBehaviour
+    {
+        public GameBootstrapper bootstrapperPrefab;
+        private void Awake()
+        {
+            var bootstrapper = FindObjectOfType<GameBootstrapper>();
+
+            if (!bootstrapper)
+            {
+                Instantiate(bootstrapperPrefab);
+            }
+        }
+    }
+}
